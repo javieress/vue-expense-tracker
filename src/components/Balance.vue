73141d39d534e89a6e71c54a1496057ae077880d
Javier Rojas <64340938+javieress@users.noreply.github.com>
2024-04-16 17:31:13 -0400

    Add Header, Balance, and AddTransaction components

---
+++ src/components/Balance.vue
@@ -0,0 +1,31 @@
+<script>
+
+export default {
+    name: 'Balance',
+}
+</script>
+
+<template>
+    <div class="balance">
+        <h4>YOUR BALANCE</h4>
+        <h1>$0.00</h1>
+    </div>
+</template>
+
+<style scoped>
+.balance {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    margin-bottom: 20px;
+}
+
+.balance h4 {
+    margin-bottom: 2px;
+}
+
+.balance h1 {
+    font-size: 40px;
+    margin: 0;
+}
+</style>
---
+++ src/components/IncomeExpenses.vue
@@ -0,0 +1,50 @@
+<script>
+export default {
+    name: 'IncomeExpenses'
+}
+</script>
+
+<template>
+    <div class="inc-exp-container">
+        <div class="income">
+            <h4>INCOME</h4>
+            <p class="money plus
+            ">+$0.00</p>
+        </div>
+        <hr>
+        <div class="expense">
+            <h4>EXPENSE</h4>
+            <p class="money minus">-$0.00</p>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.inc-exp-container {
+    display: flex;
+    justify-content: space-evenly;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    box-shadow: 0 3px 5px rgba(0, 0, 0, 0.1);
+    margin-bottom: 20px;
+    padding: 20px;
+    background-color: #fff;
+}
+
+.inc-exp-container div {
+    text-align: center;
+}
+
+.inc-exp-container div p {
+    font-size: 20px;
+    margin: 5px 0;
+}
+
+.plus {
+    color: #2ecc71;
+}
+
+.minus {
+    color: #c0392b;
+}
+</style>
